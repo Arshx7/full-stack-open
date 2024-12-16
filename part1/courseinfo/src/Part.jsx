678ba7f82d8ef729e50658e58cd08fc5f6fa1b93
Arshakh Haneen <arshx77@gmail.com>
2024-12-16 15:44:55 +0530

    Add course information step 2

---
+++ part1/courseinfo/src/Part.jsx
@@ -0,0 +1,12 @@
+function Part(props) {
+    console.log(props);
+    return (
+      <>
+        <p>
+          {props.part} {props.exercises}
+        </p>
+      </>
+    );
+  }
+  
+  export default Part;
