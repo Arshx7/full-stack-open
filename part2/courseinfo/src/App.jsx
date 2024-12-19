41c63f88519ce85bc94649a39600a2b617c4b7de
Arshakh Haneen <arshx77@gmail.com>
2024-12-19 19:30:46 +0530

    Exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,31 +1,4 @@
-const Header = ({ course }) => {
-  return <h2>{course.name}</h2>;
-};
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((s, p) => (s += p.exercises), 0);
-
-  return <h3>Total Number of exercises {total}</h3>;
-};
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => {
-  return parts.map((part) => <Part part={part} key={part.id} />);
-};
-const Course = ({ courses }) => {
-  return courses.map((course) => (
-    <div key={course.id}>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  ));
-};
+import Course from "./Course";
 
 const App = () => {
   const courses = [
